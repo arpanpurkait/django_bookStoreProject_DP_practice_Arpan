1f3952d6369db12fb4f76a88e79d2d821a31189c
Arpan Purkait <arpanpurkait743354@gmail.com>
2025-11-12 21:10:51 +0530

    final commit for chapter 11(book app)

--- django_project/settings.py
+++ django_project/settings.py
@@ -80,7 +80,9 @@ AUTHENTICATION_BACKENDS = [
     "django.contrib.auth.backends.ModelBackend",
     "allauth.account.auth_backends.AuthenticationBackend",
 ]
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
