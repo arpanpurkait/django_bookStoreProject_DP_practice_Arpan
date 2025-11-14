295dff9f5a1baa086fd31d32bed6b13529ae7cbe
Arpan Purkait <arpanpurkait743354@gmail.com>
2025-11-14 23:16:48 +0530

    final commit chapter 14

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,16 +1,28 @@
 from django.contrib import admin
-from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth import get_user_model
 from .forms import CustomUserCreationForm, CustomUserChangeForm
+
 CustomUser = get_user_model()
 
-# Register your models here.
-
-
-
+@admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = CustomUser
-    list_display = ["username", "email", "is_superuser",]
-admin.site.register(CustomUser, CustomUserAdmin)
+
+    list_display = ("username", "email", "is_staff")
+    search_fields = ("email", "username")
+    ordering = ("email",)
+
+    fieldsets = (
+        (None, {"fields": ("username", "email", "password")}),
+        ("Permissions", {"fields": ("is_staff", "is_active", "is_superuser", "groups", "user_permissions")}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("username", "email", "password1", "password2", "is_staff", "is_active"),
+        }),
+    )
--- books/views.py
+++ books/views.py
@@ -1,15 +1,16 @@
-from django.shortcuts import render
-
-# Create your views here.
+# books/views.py
+from django.contrib.auth.mixins import (LoginRequiredMixin,
+                                        PermissionRequiredMixin)
 from django.views.generic import ListView, DetailView
-from.models import Book
-
-class BookListView(ListView):
+from .models import Book
+class BookListView(LoginRequiredMixin, ListView): # new
     model = Book
+    context_object_name = "book_list"
     template_name = "books/book_list.html"
-    context_object_name = "books_list"
-
-class BookDetailView(DetailView): # new
+    login_url = "account_login" # new
+class BookDetailView(LoginRequiredMixin,PermissionRequiredMixin, DetailView): # new
     model = Book
-    context_object_name = "book" # new
+    context_object_name = "book"
     template_name = "books/book_detail.html"
+    login_url = "account_login" # new
+    permission_required = "books.special_status"
